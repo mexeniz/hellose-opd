b998341c506535e042689d50f81e44743b7c7868
Archan Soonthornarom <santazaaa@gmail.com>
2015-09-20 15:54:27 +0700

    Fix post bug

--- app.js
+++ app.js
@@ -10,16 +10,11 @@ var mongoose = require('mongoose');
 require('./models/model-patients');
 require('./models/model-physicalrecords');
 
-//Routers
-var main_routes = require('./routes/ctrl-main');
-var physical_routes = require('./routes/ctrl-physicalrecords');
-var patients_routes = require('./routes/ctrl-patients');
+
 
 var app = express();
 
-app.use('/', main_routes);
-app.use('/physical_records', physical_routes);
-app.use('/patients', patients_routes);
+
 
 mongoose.connect('mongodb://localhost/hellose-opd');
 
@@ -37,6 +32,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+//Routers
+var main_routes = require('./routes/ctrl-main');
+var physical_routes = require('./routes/ctrl-physicalrecords');
+var patients_routes = require('./routes/ctrl-patients');
+
+app.use('/', main_routes);
+app.use('/physical_records', physical_routes);
+app.use('/patients', patients_routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
