c42d296ea11a09a488b33157a1aa2b05e591a1b1
Archan Soonthornarom <santazaaa@gmail.com>
2015-09-20 18:11:38 +0700

    Add function to generate physical record

--- public/javascripts/patients_app.js
+++ public/javascripts/patients_app.js
@@ -20,13 +20,21 @@ app.factory('patients_fac', ['$http', function($http){
 		    o.patients.push(data);
 			});
 	  };
+		o.addPhysicalRecord = function(patient, pRecord)
+		{
+			return $http.post('/' + patient._id + '/physicalrecord/insert', pRecord).success(function(data){
+				console.log(data);
+		    patient.physical_record.push(data);
+			});
+		}
 	  return o;
 	}]);
 
 app.controller('ListCtrl', [
 	'$scope',
-	'patients_fac' ,
-	function($scope , patients_fac ){
+	'patients_fac',
+
+	function($scope, patients_fac){
 		patients_fac.getList();
 		$scope.patients = patients_fac.patients;
 
@@ -58,6 +66,19 @@ app.controller('ListCtrl', [
 					tel_number: [genTelNum]
 				}
 			);
+		};
+
+		$scope.generatePhysicalRecord = function(patient)
+		{
+			var pRecord = {
+				weight: Math.floor(Math.random()*50 + 50),
+				height: Math.floor(Math.random()*70 + 120),
+				blood_pressure: Math.floor(Math.random()*50 + 100),
+				pulse: Math.floor(Math.random()*30 + 30),
+				temperature: Math.floor(Math.random()*5 + 35)
+			};
+			console.log(pRecord);
+			patients_fac.addPhysicalRecord(patient, pRecord);
 		};
 	}
 ]);
