32807d61a4af68d278bf04232a0c3b36a93f9be6
Pongsatorn Santiwatanakul <kirk.kirby@hotmail.com>
2015-10-28 20:25:35 +0700

    [pharmacist] show mockup data

--- public/javascripts/pharmacist_app.js
+++ public/javascripts/pharmacist_app.js
@@ -3,13 +3,25 @@ var app = angular.module('prescriptions', ['ui.router']) ;
 
 app.factory('prescriptions_fac', ['$http', function($http){
 	  var o = {
-	  	prescriptions : [{"date":'20/10/58',"patient_name":'นายก',"patient_lastname":'ตู่',"status":"รอการจ่าย"}]
+	  	prescriptions : [
+	  	{"date":'20/10/58',"patient_name":'นายก',"patient_lastname":'ตู่',"status":"รอการจ่าย"},
+	  	{"date":'21/11/58',"patient_name":'เอ็มม่า',"patient_lastname":'ราชาลิง',"status":"จ่ายแล้ว"}]
 	  };
 	  
-	  var getList = function(){
+	  o.getList = function(){
 	  	return o.prescriptions;
 	  };
 
 	  return o;
 	}]);
+
+app.controller('ListCtrl', [
+	'$scope',
+	'prescriptions_fac',
+	function($scope, prescriptions_fac){
+		prescriptions_fac.getList();
+		$scope.prescriptions = prescriptions_fac.prescriptions;
+	}
+]);
+
 })();
