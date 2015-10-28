f401162f1577dbfd469a411c31c5f2f595d272f4
Pongsatorn Santiwatanakul <kirk.kirby@hotmail.com>
2015-10-28 21:23:18 +0700

    [pharmacist] - sort by date - filter by input and dropdown

--- public/javascripts/pharmacist_app.js
+++ public/javascripts/pharmacist_app.js
@@ -4,8 +4,11 @@ var app = angular.module('prescriptions', ['ui.router']) ;
 app.factory('prescriptions_fac', ['$http', function($http){
 	  var o = {
 	  	prescriptions : [
-	  	{"date":'20/10/58',"patient_name":'นายก',"patient_lastname":'ตู่',"status":"รอการจ่าย"},
-	  	{"date":'21/11/58',"patient_name":'เอ็มม่า',"patient_lastname":'ราชาลิง',"status":"จ่ายแล้ว"}]
+	  	{"date":100,"patient_name":'นายก',"patient_lastname":'ตู่',"status":"รอการจ่าย"},
+	  	{"date":200,"patient_name":'เอ็มม่า',"patient_lastname":'ราชาลิง',"status":"จ่ายแล้ว"},
+	  	{"date":150,"patient_name":'แมวน้ำ',"patient_lastname":'เอฟซี',"status":"รอการจ่าย"},
+	  	{"date":250,"patient_name":'mike',"patient_lastname":'miller',"status":"จ่ายแล้ว"},
+	  	]
 	  };
 	  
 	  o.getList = function(){
@@ -21,7 +24,30 @@ app.controller('ListCtrl', [
 	function($scope, prescriptions_fac){
 		prescriptions_fac.getList();
 		$scope.prescriptions = prescriptions_fac.prescriptions;
+
+		$scope.statusFilterOption = {
+			stores : [
+				{id : 0, name : 'ทั้งหมด'},
+				{id : 1, name : 'รอการจ่าย'},
+				{id : 2, name : 'จ่ายแล้ว'}	
+			]
+		};
+
+		$scope.statusFilterItem = {
+			store : $scope.statusFilterOption.stores[0]
+		};
+
+		$scope.statusFilter = function(data){
+			if ($scope.statusFilterItem.store.name === 'ทั้งหมด') {
+		    	return true;
+		    } else if(data.status === $scope.statusFilterItem.store.name){
+				return true;
+			}else {
+		    	return false;
+		  	}
+		};
 	}
+
 ]);
 
 })();
