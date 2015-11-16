84a49cc7bb055ae75f79c9b8cc4166f79164ca8e
Theppasith N <tutorgaming@gmail.com>
2015-11-16 15:16:21 +0700

    Add Class Routing to Ctrl-Appointment.js

--- routes/ctrl-appointment.js
+++ routes/ctrl-appointment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/**
+/*
 Router reference 
 router.METHOD(path, callback [, callback ...])
 */ 
@@ -15,10 +15,20 @@ module.exports = router;
 var mongoose = require('mongoose');
 var Appointment = mongoose.model('Appointment');
 
+////////////////////////////////////////////
+//Class Required
+////////////////////////////////////////////
+var UserControl = require('../controllers/UserControl.js');
+var RoundWardControl = require('../controllers/RoundWardControl.js');
+var NotificationControl = require('../controllers/NotificationControl.js');
+var AppointmentControl = require('../controllers/AppointmentControl.js');
 
 
-//Get Request on the root of this controller
-router.get('/', function(req,res){
+////////////////////////////////////////////
+//PAGE REQUEST under URL = '/appointment'
+////////////////////////////////////////////
+
+router.get('/', function(req,res){ //Default Route
 	//Callback function on get finished
 	//Render index.ejs
 	res.render('appointment/index');
@@ -36,31 +46,105 @@ router.get('/patientView', function(req,res){
 });
 
 
-//POST Function To Create New Entry
-router.post('/insert',function(req,res,next){
-	//Since Everything in appointment are packed in req.body
-	var appointment = new Appointment(req.body);
-	//Save Things in to Database via mongoose api
-	Appointment.save(function(err, appointment) {
-		//onError do nothing and return error msg 
-		if(err){
-			return next(err);
-		}
-		//onSuccess Return itself in json packed formatted
-		res.json(appointment);
-	});
 
-});
 
-//GET Function to fetch All list  
-router.get('/getAll',function(req,res,next) {
-	//Populate Everything
-	Appointment.find(function(err,appointment){
-		if(err){
-			return next(err);
-		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////
+
+// //POST Function To Create New Entry
+// router.post('/insert',function(req,res,next){
+// 	//Since Everything in appointment are packed in req.body
+// 	var appointment = new Appointment(req.body);
+// 	//Save Things in to Database via mongoose api
+// 	Appointment.save(function(err, appointment) {
+// 		//onError do nothing and return error msg 
+// 		if(err){
+// 			return next(err);
+// 		}
+// 		//onSuccess Return itself in json packed formatted
+// 		res.json(appointment);
+// 	});
+
+// });
+
+// //GET Function to fetch All list  
+// router.get('/getAll',function(req,res,next) {
+// 	//Populate Everything
+// 	Appointment.find(function(err,appointment){
+// 		if(err){
+// 			return next(err);
+// 		}
 	
-	});
+// 	});
 
 
-});
+// });
