8d5a0962fe768831ff1f457eaf538a5d24a84dad
ArchSanta <santazaaa@gmail.com>
2015-11-19 20:01:28 +0700

    - Added appointment controller template and model

--- models/model-appointment.js
+++ models/model-appointment.js
@@ -12,7 +12,11 @@ Schema = mongoose.Schema;
 
 //Create Appointment Schema
 var AppointmentSchema = new Schema({
-	
+	patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+	doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor' },
+	roundWard: { type: mongoose.Schema.Types.ObjectId, ref: 'RoundWard' },
+	date: Date,
+	time: String
 });
 
 module.exports = mongoose.model('Appointment', AppointmentSchema);
